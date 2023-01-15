d30836656f6de5ac149cb20ca2b75faca2d10b40
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2023-01-15 20:22:59 +0100

    Disable typing on Rakefile.rb

--- api/Rakefile.rb
+++ api/Rakefile.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
-# typed: true
+# typed: false
 
 # Add your own tasks in files placed in lib/tasks ending in .rake,
 # for example lib/tasks/capistrano.rake, and they will automatically be available to Rake.
