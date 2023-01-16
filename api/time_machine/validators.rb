02e2a3cfe5bfa40b0e29d724f3c6f52695e63e70
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2023-01-16 15:50:08 +0100

    Fix tests

--- api/time_machine/validators.rb
+++ api/time_machine/validators.rb
@@ -4,7 +4,8 @@
 require 'sorbet-runtime'
 require './time_machine/types'
 require './time_machine/watches'
-
+require 'active_support'
+require 'active_support/core_ext'
 
 module Validators
   extend T::Sig
@@ -224,7 +225,7 @@ module Validators
         Watches.match_osm_filters_tags(@watches, after['tags'])
       ).intersection(diff.tags.keys).select{ |tag|
         # Exclude new tags with insignificant value
-        !(before['tags'].exclude?(tag) && after['tags'].include?(tag) && after['tags'][tag] == 'no')
+        !before || !(before['tags'].exclude?(tag) && after['tags'].include?(tag) && after['tags'][tag] == 'no')
       }
       match_keys.each{ |key|
         assign_action_reject(diff.tags[key])
