0ef9e3d1be0574a340520abac1246ce8f058726f
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2023-08-26 11:05:28 +0200

    Make update more safe

--- scripts/update.sh
+++ scripts/update.sh
@@ -16,25 +16,21 @@ for PROJECT in $PROJECTS; do
 
     if flock -n -x 8; then
         # Get Update
-        [ ! -f ${IMPORT}/diff.osc.xml.bz2 ] && \
+        [ ! -f ${IMPORT}/diff.osc.xml.bz2 ] && [ ! -f ${IMPORT}/osm_changes.pgcopy ] && \
         osmosis --read-replication-interval workingDirectory=${IMPORT}/replication --write-xml-change ${IMPORT}/diff.osc.xml.bz2
         # Convert
-        docker-compose --env-file .tools.env run --rm ope ope -H /${IMPORT}/diff.osc.xml.bz2 /${IMPORT}/osm_changes=o
+        [ ! -f ${IMPORT}/osm_changes.pgcopy ] && \
+        docker-compose --env-file .tools.env run --rm ope ope -H /${IMPORT}/diff.osc.xml.bz2 /${IMPORT}/osm_changes=o && \
+        rm -f ${IMPORT}/diff.osc.xml.bz2
         # Import
-        docker-compose exec -T -u postgres postgres psql -v ON_ERROR_STOP=ON -c "\copy ${PROJECT_NAME}.osm_changes from '/${IMPORT}/osm_changes.pgcopy'"
-        rm -f ${IMPORT}/diff.osc.xml.bz2 ${IMPORT}/osm_changes.pgcopy
+        docker-compose exec -T -u postgres postgres psql -v ON_ERROR_STOP=ON -c "\copy ${PROJECT_NAME}.osm_changes from '/${IMPORT}/osm_changes.pgcopy'" && \
+        rm -f ${IMPORT}/osm_changes.pgcopy
 
         # Validation report
-        echo == changes-prune ==
-        docker-compose --env-file .tools.env run --rm api ruby time_machine/main.rb --project=/${PROJECT} --changes-prune
-        echo == apply_unclibled_changes ==
-        docker-compose --env-file .tools.env run --rm api ruby time_machine/main.rb --project=/${PROJECT} --apply_unclibled_changes
-        echo == fetch_changesets ==
-        docker-compose --env-file .tools.env run --rm api ruby time_machine/main.rb --project=/${PROJECT} --fetch_changesets
-        echo == validate ==
-        docker-compose --env-file .tools.env run --rm api ruby time_machine/main.rb --project=/${PROJECT} --validate
-
-        # Export diff
+        docker-compose --env-file .tools.env run --rm api ruby time_machine/main.rb --project=/${PROJECT} --changes-prune && \
+        docker-compose --env-file .tools.env run --rm api ruby time_machine/main.rb --project=/${PROJECT} --apply_unclibled_changes && \
+        docker-compose --env-file .tools.env run --rm api ruby time_machine/main.rb --project=/${PROJECT} --fetch_changesets && \
+        docker-compose --env-file .tools.env run --rm api ruby time_machine/main.rb --project=/${PROJECT} --validate && \
         docker-compose --env-file .tools.env run --rm api ruby time_machine/main.rb --project=/${PROJECT} --export-osm-update
     else
         echo "${PROJECT} Update already locked"
