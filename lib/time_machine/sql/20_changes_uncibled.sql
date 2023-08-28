3f99f7bb05750c65f77abd8ae47ba87aeb785e68
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2023-08-28 15:59:06 +0200

    Let pass geom less object

--- lib/time_machine/sql/20_changes_uncibled.sql
+++ lib/time_machine/sql/20_changes_uncibled.sql
@@ -15,6 +15,7 @@ base_update AS (
         osm_base,
         clip
     WHERE
+        osm_base.geom IS NULL OR
         NOT (:osm_filter_tags) OR
         (:polygon IS NOT NULL AND NOT ST_Intersects(clip.geom, osm_base.geom))
 ),
@@ -25,6 +26,7 @@ changes AS (
         osm_changes_geom,
         clip
     WHERE
+        osm_changes_geom.geom IS NULL OR
         NOT (:osm_filter_tags) OR
         (:polygon IS NOT NULL AND NOT ST_Intersects(clip.geom, osm_changes_geom.geom))
 )
