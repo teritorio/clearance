db394867f0f5e789f1b44461f2d59489ca80be66
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2024-04-04 21:44:31 +0200

    Use sql from overpass_parser

--- lib/time_machine/db/overpasslike.rb
+++ lib/time_machine/db/overpasslike.rb
@@ -27,87 +27,35 @@ module Db
 
     sig {
       params(
-        query: String,
-      ).returns(T::Array[[OverpassParser::Selectors, T.nilable(T::Array[Integer])]])
-    }
-    def self.parse(query)
-      # Crud overpass extraction of tag selector of nwr line like
-      # [out:json][timeout:25];
-      # area(id:#{area_ids})->.a;
-      # (
-      #   nwr[selectors](area.a);
-      # );
-      # out center meta;
-
-      tree = OverpassParser.tree(query)
-
-      raise unless tree[0][:queries].size == 2
-
-      raise unless tree[0][:queries][0][:type] == :query_object
-      raise unless tree[0][:queries][0][:object_type] == 'area'
-      raise unless tree[0][:queries][0][:filters].size == 1
-
-      area_ids = tree[0][:queries][0][:filters][0][:ids]
-      raise if area_ids.nil?
-
-      area_ids = area_ids.collect{ |i| i.to_i - 3_600_000_000 }
-
-      raise unless tree[0][:queries][1][:type] == :query_group
-
-      tree[0][:queries][1][:queries].collect{ |q|
-        raise unless q[:type] == :query_object
-        raise unless q[:object_type] == 'nwr'
-
-        [q[:selectors], area_ids]
-      }
-    end
-
-    sig {
-      params(
         conn: PG::Connection,
         query: String,
       ).returns(T::Array[T::Hash[Symbol, OverpassResult]])
     }
     def self.query(conn, query)
-      parse(query).collect{ |selector, area_ids|
-        db_query(conn, selector, area_ids)
-      }.flatten(1).uniq
-    end
-
-    sig {
-      params(
-        conn: PG::Connection,
-        selector: OverpassParser::Selectors,
-        area_ids: T.nilable(T::Array[Integer]),
-      ).returns(T::Array[T::Hash[Symbol, OverpassResult]])
-    }
-    def self.db_query(conn, selector, area_ids)
-      sql_osm_filter_tags = selector.to_sql(->(s) { conn.escape_literal(s) })
+      request = OverpassParser.tree(query)[0]
       sql = File.new('/sql/overpasslike.sql').read
-                .gsub(':osm_filter_tags', sql_osm_filter_tags)
-                .gsub(':area_ids', conn.escape_literal(area_ids.to_s))
+      sql += request.to_sql(->(s) { conn.escape_literal(s) }).gsub('ST_PointOnSurface(geom) AS geom', 'ST_X(ST_PointOnSurface(geom)) AS lon, ST_Y(ST_PointOnSurface(geom)) AS lat')
+
       conn.exec(sql) { |result|
         result.collect{ |row|
-          object = T.let(OverpassResult.from_hash(row), OverpassResult)
-
           ret = {
-            id: object.id,
-            version: object.version,
-            timestamp: object.timestamp,
-            tags: object.tags,
+            id: row['id'],
+            version: row['version'],
+            timestamp: row['created'],
+            tags: row['tags'],
           }
-          if object.objtype == 'n'
+          if row['osm_type'] == 'n'
             ret.update({
               type: 'node',
-              lat: T.must(object.lat),
-              lon: T.must(object.lon),
+              lat: T.must(row['lat']),
+              lon: T.must(row['lon']),
             })
           else
             ret.update({
-              type: object.objtype == 'w' ? 'way' : 'relation',
+              type: row['osm_type'] == 'w' ? 'way' : 'relation',
               center: {
-                lat: T.must(object.lat),
-                lon: T.must(object.lon),
+                lat: T.must(row['lat']),
+                lon: T.must(row['lon']),
               },
             })
           end
--- lib/time_machine/sql/overpasslike.sql
+++ lib/time_machine/sql/overpasslike.sql
@@ -1,31 +1,10 @@
-SELECT
-  objtype,
-  id,
-  version,
-  created AS timestamp,
-  tags,
-  ST_X(
-    CASE objtype
-      WHEN 'n' THEN geom
-      WHEN 'w' THEN ST_LineInterpolatePoint(geom, 0.5)
-      WHEN 'r' THEN ST_Centroid(geom)
-    END) AS lon,
-  ST_Y(
-    CASE objtype
-      WHEN 'n' THEN geom
-      WHEN 'w' THEN ST_LineInterpolatePoint(geom, 0.5)
-      WHEN 'r' THEN ST_Centroid(geom)
-    END) AS lat
-FROM
-  osm_base
-WHERE
-  (:osm_filter_tags) AND
-  (
-    :area_ids IS NULL
-    OR
-    ST_Intersects(
-      geom,
-      (SELECT ST_Union(geom) AS polygon FROM osm_base_areas WHERE id = ANY(replace(replace(:area_ids, '[', '{'), ']', '}')::int[]))
-    )
-  )
-;
+CREATE TEMP VIEW area AS
+SELECT id, version, created, tags, NULL::bigint[] AS nodes, NULL::jsonb AS members, geom, 'a' AS osm_type FROM osm_base_areas;
+CREATE TEMP VIEW node AS
+SELECT id, version, created, tags, NULL::bigint[] AS nodes, NULL::jsonb AS members, geom, objtype AS osm_type FROM osm_base WHERE objtype = 'n';
+CREATE TEMP VIEW way AS
+SELECT id, version, created, tags, nodes, NULL::jsonb AS members, geom, objtype AS osm_type FROM osm_base WHERE objtype = 'w';
+CREATE TEMP VIEW relation AS
+SELECT id, version, created, tags, NULL::bigint[] AS nodes, members, geom, objtype AS osm_type FROM osm_base WHERE objtype = 'r';
+CREATE TEMP VIEW nwr AS
+SELECT id, version, created, tags, nodes, members, geom, objtype AS osm_type FROM osm_base;
