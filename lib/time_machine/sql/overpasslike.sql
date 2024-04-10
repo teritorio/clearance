9c59db16a92f6928292597b9d399a09672a51c0a
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2024-04-10 12:28:02 +0200

    Update overpass sql

--- lib/time_machine/sql/overpasslike.sql
+++ lib/time_machine/sql/overpasslike.sql
@@ -1,10 +1,14 @@
-CREATE TEMP VIEW area AS
-SELECT id, version, created, tags, NULL::bigint[] AS nodes, NULL::jsonb AS members, geom, 'a' AS osm_type FROM osm_base_areas;
-CREATE TEMP VIEW node AS
+CREATE OR REPLACE TEMP VIEW node AS
 SELECT id, version, created, tags, NULL::bigint[] AS nodes, NULL::jsonb AS members, geom, objtype AS osm_type FROM osm_base WHERE objtype = 'n';
-CREATE TEMP VIEW way AS
+
+CREATE OR REPLACE TEMP VIEW way AS
 SELECT id, version, created, tags, nodes, NULL::jsonb AS members, geom, objtype AS osm_type FROM osm_base WHERE objtype = 'w';
-CREATE TEMP VIEW relation AS
+
+CREATE OR REPLACE TEMP VIEW relation AS
 SELECT id, version, created, tags, NULL::bigint[] AS nodes, members, geom, objtype AS osm_type FROM osm_base WHERE objtype = 'r';
-CREATE TEMP VIEW nwr AS
+
+CREATE OR REPLACE TEMP VIEW nwr AS
 SELECT id, version, created, tags, nodes, members, geom, objtype AS osm_type FROM osm_base;
+
+CREATE OR REPLACE TEMP VIEW area AS
+SELECT id + CASE objtype[1] WHEN 'r' THEN 3600000000 ELSE 0 END, version, created, tags, NULL::bigint[] AS nodes, NULL::jsonb AS members, geom, CASE objtype[1] WHEN 'w' THEN 'w' ELSE 'a' END AS osm_type FROM osm_base_areas;
