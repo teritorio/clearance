15d12b10a2cdb72c946acb781f1585a2500b095c
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2024-05-23 21:37:45 +0200

    Fix update-schema script

--- bin/update-schema.sh
+++ bin/update-schema.sh
@@ -13,6 +13,7 @@ for PROJECT in $PROJECTS; do
     # Check if schema/table exist
     psql $DATABASE_URL -v ON_ERROR_STOP=ON -c "SELECT * FROM ${PROJECT}.osm_changes LIMIT 1" 2&> /dev/null && {
         psql $DATABASE_URL -v ON_ERROR_STOP=ON -v schema=${PROJECT} -f lib/time_machine/sql/schema/schema_changes_geom.sql
+        psql $DATABASE_URL -v ON_ERROR_STOP=ON -v schema=${PROJECT} -f lib/time_machine/sql/schema/schema_geom.sql
         psql $DATABASE_URL -v ON_ERROR_STOP=ON -v schema=${PROJECT} -f lib/time_machine/sql/changes_logs.sql
     } || echo "Fails to update non initialized project $PROJECT"
 done
