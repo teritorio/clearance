febd2e565d666e406fdbf59c30b8f3cdcf7a8de0
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2024-08-18 16:36:42 +0200

    Fix sql perf issue

--- lib/time_machine/sql/20_changes_uncibled.sql
+++ lib/time_machine/sql/20_changes_uncibled.sql
@@ -1,3 +1,23 @@
+DROP TABLE IF EXISTS tmp_changes;
+CREATE TEMP TABLE tmp_changes AS
+WITH
+clip AS (
+    SELECT
+        ST_Union(ST_GeomFromGeoJSON(geom)) AS geom
+    FROM
+        json_array_elements_text(:polygon::json) AS t(geom)
+)
+SELECT
+    osm_changes_geom.*
+FROM
+    osm_changes_geom,
+    clip
+WHERE
+    osm_changes_geom.geom IS NULL OR
+    NOT (:osm_filter_tags) OR
+    (:polygon IS NOT NULL AND NOT ST_Intersects(clip.geom, osm_changes_geom.geom))
+;
+
 DROP TABLE IF EXISTS changes_update;
 CREATE TEMP TABLE changes_update AS
 WITH
@@ -19,23 +39,12 @@ base_update AS (
         osm_base.geom IS NULL OR
         NOT (:osm_filter_tags) OR
         (:polygon IS NOT NULL AND NOT ST_Intersects(clip.geom, osm_base.geom))
-),
-changes AS (
-    SELECT
-        osm_changes_geom.*
-    FROM
-        osm_changes_geom,
-        clip
-    WHERE
-        osm_changes_geom.geom IS NULL OR
-        NOT (:osm_filter_tags) OR
-        (:polygon IS NOT NULL AND NOT ST_Intersects(clip.geom, osm_changes_geom.geom))
 )
 SELECT DISTINCT ON (changes.objtype, changes.id)
     changes.*
 FROM
     base_update AS base
-    JOIN changes ON
+    JOIN tmp_changes AS changes ON
         changes.objtype = base.objtype AND
         changes.id = base.id
 ORDER BY
@@ -44,3 +53,5 @@ ORDER BY
     changes.version DESC,
     changes.deleted DESC
 ;
+
+DROP TABLE tmp_changes;
