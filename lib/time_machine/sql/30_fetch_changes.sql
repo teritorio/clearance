3dc8dc312a71e957bece3c0eba001678facaaa14
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2024-08-21 15:28:44 +0200

    SQL fetch_changes() perf

--- lib/time_machine/sql/30_fetch_changes.sql
+++ lib/time_machine/sql/30_fetch_changes.sql
@@ -6,123 +6,146 @@ CREATE OR REPLACE FUNCTION fetch_changes(
     id bigint,
     geom geometry(Geometry, 4326),
     p jsonb
-) AS $$
-WITH
-    polygons AS (
-        SELECT
-            row_json->>0 AS group_id,
-            ST_GeomFromGeoJSON(row_json->>1) AS geom
-        FROM
-            jsonb_array_elements(group_id_polys) AS t(row_json)
-    ),
-    change_uniq AS (
-        SELECT
-            objtype,
-            id
-        FROM
-            osm_changes
-        GROUP BY
-            objtype,
-            id
-    ),
-    base_i AS (
-        SELECT
-            base.objtype,
-            base.id,
-            base.version,
-            false AS deleted,
-            base.created,
-            base.uid,
-            base.username,
-            base.tags,
-            base.lon,
-            base.lat,
-            base.nodes,
-            base.members,
-            base.geom
-        FROM
-            osm_base AS base
-            JOIN change_uniq ON
-                change_uniq.objtype = base.objtype AND
-                change_uniq.id = base.id
-    ),
-    changes_with_changesets AS (
-        SELECT DISTINCT ON (objtype, id)
-            osm_changes_geom.objtype,
-            osm_changes_geom.id,
-            osm_changes_geom.version,
-            osm_changes_geom.deleted,
-            osm_changes_geom.created,
-            osm_changes_geom.uid,
-            osm_changes_geom.username,
-            osm_changes_geom.tags,
-            osm_changes_geom.lon,
-            osm_changes_geom.lat,
-            osm_changes_geom.nodes,
-            osm_changes_geom.members,
-            osm_changes_geom.geom,
-            (
-                SELECT
-                    json_agg(row_to_json(osm_changesets) ORDER BY osm_changesets.id)
-                FROM
-                    osm_changes
-                    JOIN osm_changesets ON
-                        osm_changesets.id = osm_changes.changeset_id
-                WHERE
-                    osm_changes.objtype = osm_changes_geom.objtype AND
-                    osm_changes.id = osm_changes_geom.id
-            ) AS changesets
-        FROM
-            osm_changes_geom
-        ORDER BY
-            objtype,
-            id,
-            version DESC,
-            deleted DESC
-    ),
-    state AS (
-        SELECT
-            objtype,
-            id,
-            version,
-            deleted,
-            created,
-            username,
-            tags,
-            members,
-            geom,
-            changesets,
-            coalesce(ST_HausdorffDistance(
-                ST_Transform((first_value(CASE WHEN NOT is_change THEN geom END) OVER (PARTITION BY objtype, id ORDER BY is_change, version, deleted)), 2154),
-                ST_Transform(geom, 2154)
-            ), 0) AS geom_distance,
-            is_change,
-            (SELECT array_agg(group_id) FROM polygons WHERE ST_Intersects(t.geom, polygons.geom)) AS group_ids
-        FROM (
-                SELECT *, NULL::json AS changesets, false AS is_change FROM base_i
+) AS $$ BEGIN
+    DROP TABLE IF EXISTS _changes;
+    CREATE TEMP TABLE _changes AS
+    SELECT
+        *
+    FROM
+        osm_changes_geom
+    ;
+
+    DROP TABLE IF EXISTS _changesets;
+    CREATE TEMP TABLE _changesets AS
+    SELECT
+        _changes.objtype,
+        _changes.id,
+        json_agg(row_to_json(osm_changesets) ORDER BY osm_changesets.id) AS changesets
+    FROM
+        _changes
+        LEFT JOIN osm_changesets ON
+            osm_changesets.id = _changes.changeset_id
+    WHERE
+        osm_changesets.id IS NOT NULL
+    GROUP BY
+        _changes.objtype,
+        _changes.id
+    ;
+    CREATE INDEX _changesets_idx ON _changesets (objtype, id);
+
+    DROP TABLE IF EXISTS change_uniq;
+    CREATE TEMP TABLE change_uniq AS
+    SELECT DISTINCT ON (c.objtype, c.id)
+        c.objtype,
+        c.id,
+        c.version,
+        c.deleted,
+        c.created,
+        c.uid,
+        c.username,
+        c.tags,
+        c.lon,
+        c.lat,
+        c.nodes,
+        c.members,
+        c.geom
+    FROM
+        _changes AS c
+    ORDER BY
+        c.objtype,
+        c.id,
+        c.version DESC,
+        c.deleted DESC
+    ;
+
+    DROP TABLE _changes;
+
+    RETURN QUERY
+    WITH
+        polygons AS (
+            SELECT
+                row_json->>0 AS group_id,
+                ST_GeomFromGeoJSON(row_json->>1) AS geom
+            FROM
+                jsonb_array_elements(group_id_polys) AS t(row_json)
+        ),
+        base_i AS (
+            SELECT
+                base.objtype,
+                base.id,
+                base.version,
+                false AS deleted,
+                base.created,
+                base.uid,
+                base.username,
+                base.tags,
+                base.lon,
+                base.lat,
+                base.nodes,
+                base.members,
+                base.geom,
+                NULL::json AS changesets,
+                false AS is_change
+            FROM
+                osm_base AS base
+                JOIN change_uniq ON
+                    change_uniq.objtype = base.objtype AND
+                    change_uniq.id = base.id
+        ),
+        changes_with_changesets AS (
+            SELECT
+                change_uniq.*,
+                changesets,
+                true AS is_change
+            FROM
+                change_uniq
+                LEFT JOIN _changesets ON
+                    change_uniq.objtype = _changesets.objtype AND
+                    change_uniq.id = _changesets.id
+        ),
+        state AS (
+            SELECT
+                t.objtype,
+                t.id,
+                t.version,
+                t.deleted,
+                t.created,
+                t.username,
+                t.tags,
+                t.members,
+                t.geom,
+                t.changesets,
+                coalesce(ST_HausdorffDistance(
+                    ST_Transform((first_value(CASE WHEN NOT t.is_change THEN t.geom END) OVER (PARTITION BY t.objtype, t.id ORDER BY t.is_change, t.version, t.deleted)), 2154),
+                    ST_Transform(t.geom, 2154)
+                ), 0) AS geom_distance,
+                t.is_change,
+                (SELECT array_agg(group_id) FROM polygons WHERE ST_Intersects(t.geom, polygons.geom)) AS group_ids
+            FROM (
+                SELECT * FROM base_i
                 UNION ALL
-                SELECT *, true AS is_change FROM changes_with_changesets
+                SELECT * FROM changes_with_changesets
             ) AS t
-        ORDER BY
-            objtype,
-            id,
-            is_change -- alows to replay histroy and keep changes after base
-    )
-SELECT
-    objtype,
-    id,
-    ST_Union(geom) AS geom,
-    json_agg(row_to_json(state)::jsonb - 'objtype' - 'id')::jsonb AS p
-FROM
-    state
-GROUP BY
-    objtype,
-    id
-ORDER BY
-    objtype,
-    id
-;
-$$ LANGUAGE SQL PARALLEL SAFE;
+            ORDER BY
+                t.objtype,
+                t.id,
+                t.is_change -- alows to replay histroy and keep changes after base
+        )
+    SELECT
+        s.objtype,
+        s.id,
+        ST_Union(s.geom) AS geom,
+        json_agg(row_to_json(s)::jsonb - 'objtype' - 'id')::jsonb AS p
+    FROM
+        state AS s
+    GROUP BY
+        s.objtype,
+        s.id
+    ORDER BY
+        s.objtype,
+        s.id
+    ;
+END; $$ LANGUAGE plpgsql PARALLEL SAFE;
 
 
 DROP FUNCTION IF EXISTS fetch_locha_changes();
