6d91c1c31871994b8f1858ff21cdc33dc338b637
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2024-11-28 15:45:39 +0100

    Lock for update-schema

--- bin/update-schema.sh
+++ bin/update-schema.sh
@@ -10,10 +10,15 @@ for PROJECT in $PROJECTS; do
     echo $PROJECT
     echo
 
+    exec {LOCK_FD}> ${PROJECT}/lock
+    flock $LOCK_FD
+
     # Check if schema/table exist
     psql $DATABASE_URL -v ON_ERROR_STOP=ON -c "SELECT * FROM ${PROJECT}.osm_changes LIMIT 1" 2&> /dev/null && {
         psql $DATABASE_URL -v ON_ERROR_STOP=ON -v schema=${PROJECT} -f lib/time_machine/sql/schema/schema_changes_geom.sql
         psql $DATABASE_URL -v ON_ERROR_STOP=ON -v schema=${PROJECT} -f lib/time_machine/sql/schema/schema_geom.sql
         psql $DATABASE_URL -v ON_ERROR_STOP=ON -v schema=${PROJECT} -f lib/time_machine/sql/changes_logs.sql
     } || echo "Fails to update non initialized project $PROJECT"
+
+    exec {LOCK_FD}>&-
 done
