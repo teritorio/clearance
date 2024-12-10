f1d4ad2a6bd5792095bc248d0adaa1195586b1c1
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2024-12-10 13:47:45 +0100

    Ensure field order

--- lib/time_machine/sql/00_import_changes.sql
+++ lib/time_machine/sql/00_import_changes.sql
@@ -1,15 +1,56 @@
 CREATE TEMP TABLE osm_changes_import (LIKE osm_changes);
 ALTER TABLE osm_changes_import DROP COLUMN cibled;
 
-COPY osm_changes_import FROM ':pgcopy';
+COPY osm_changes_import(
+    objtype,
+    id,
+    version,
+    deleted,
+    changeset_id,
+    created,
+    uid,
+    username,
+    tags,
+    lon,
+    lat,
+    nodes,
+    members
+)
+FROM ':pgcopy';
 
 DO $$ BEGIN
     RAISE NOTICE '00_import_changes - COPY: %', (SELECT COUNT(*) FROM osm_changes_import);
 END; $$ LANGUAGE plpgsql;
 
-INSERT INTO osm_changes
+INSERT INTO osm_changes(
+    objtype,
+    id,
+    version,
+    deleted,
+    changeset_id,
+    created,
+    uid,
+    username,
+    tags,
+    lon,
+    lat,
+    nodes,
+    members
+)
 SELECT DISTINCT ON (id, objtype)
-    *
+    objtype,
+    id,
+    version,
+    deleted,
+    changeset_id,
+    created,
+    uid,
+    username,
+    tags,
+    lon,
+    lat,
+    nodes,
+    members
 FROM
     osm_changes_import
 ORDER BY
