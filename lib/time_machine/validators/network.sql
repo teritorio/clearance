30e0f8d018fd64b7935f2f5d5003273c1cef8b8e
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2026-03-20 16:29:05 +0100

    Network SQL refactoring

--- lib/time_machine/validators/network.sql
+++ lib/time_machine/validators/network.sql
@@ -5,63 +5,93 @@ CREATE OR REPLACE FUNCTION validator_network_nodes_intersection(n1_ids BIGINT[],
   SELECT unnest(n2_ids) AS node_id
 $$ LANGUAGE SQL PARALLEL SAFE;
 
-CREATE TEMP VIEW validator_network AS
+CREATE OR REPLACE TEMP VIEW base AS
+SELECT
+  *
+FROM
+  osm_base_w
+WHERE
+  id = ANY(:base_ways_ids) AND
+  (:osm_filter_tags)
+;
+
+CREATE OR REPLACE TEMP VIEW changes AS
+SELECT
+  *
+FROM
+  osm_changes
+WHERE
+  objtype = 'w' AND
+  id = ANY(:change_ways_ids) AND
+  (:osm_filter_tags)
+;
+
+DROP TABLE IF EXISTS base_connection CASCADE;
+CREATE TEMP TABLE base_connection AS
+SELECT
+  way.id,
+  way.nodes,
+  validator_network_nodes_intersection(way.nodes, way_other.nodes) AS node_id
+FROM
+  base AS way
+  JOIN osm_base_w AS way_other ON
+    NOT way_other.id = ANY(:base_ways_ids) AND
+    way.nodes && way_other.nodes AND
+    (:osm_filter_tags)
+WHERE
+  way.id = ANY(:base_ways_ids)
+;
+CREATE INDEX ON base_connection USING btree(node_id);
+
+DROP TABLE IF EXISTS changes_connection CASCADE;
+CREATE TEMP TABLE changes_connection AS
+SELECT
+  way.id,
+  way.nodes,
+  validator_network_nodes_intersection(way.nodes, way_other.nodes) AS node_id
+FROM
+  changes AS way
+  JOIN osm_base_w AS way_other ON
+    NOT way_other.id = ANY(:change_ways_ids) AND
+    way.nodes && way_other.nodes AND
+    (:osm_filter_tags)
+;
+CREATE INDEX ON changes_connection USING btree(node_id);
+
+CREATE TEMP VIEW lost_connection AS
 WITH
-base AS (
-  SELECT
-    way.id,
-    validator_network_nodes_intersection(way.nodes, way_other.nodes) AS node_id
-  FROM
-    osm_base_w AS way
-    JOIN osm_base_w AS way_other ON
-      NOT way_other.id = ANY(:base_ways_ids) AND
-      way.nodes && way_other.nodes AND
-      (:osm_filter_tags)
-  WHERE
-    way.id = ANY(:base_ways_ids)
-),
-change AS (
-  SELECT
-    way.id,
-    validator_network_nodes_intersection(way.nodes, way_other.nodes) AS node_id
-  FROM
-    osm_changes AS way
-    JOIN osm_base_w AS way_other ON
-      NOT way_other.id = ANY(:change_ways_ids) AND
-      way.nodes && way_other.nodes AND
-      (:osm_filter_tags)
-  WHERE
-    way.objtype = 'w' AND
-    way.id = ANY(:change_ways_ids)
-),
 nodes_ids AS (
-  (SELECT true AS lost_connection, node_id FROM ((SELECT DISTINCT node_id FROM base) EXCEPT (SELECT DISTINCT node_id FROM change)) AS t)
+  (SELECT true AS lost_connection, node_id FROM ((SELECT DISTINCT node_id FROM base_connection) EXCEPT (SELECT DISTINCT node_id FROM changes_connection)) AS t)
   UNION ALL
-  (SELECT false AS lost_connection, node_id FROM ((SELECT DISTINCT node_id FROM change) EXCEPT (SELECT DISTINCT node_id FROM base)) AS t)
+  (SELECT false AS lost_connection, node_id FROM ((SELECT DISTINCT node_id FROM changes_connection) EXCEPT (SELECT DISTINCT node_id FROM base_connection)) AS t)
 )
 (
-SELECT DISTINCT ON (base.id, nodes_ids.node_id)
-  base.id,
+SELECT DISTINCT ON (base_connection.id, nodes_ids.node_id)
+  base_connection.id,
   true AS base,
   nodes_ids.lost_connection,
   nodes_ids.node_id
 FROM
-  base
+  base_connection
   JOIN nodes_ids USING (node_id)
 ORDER BY
-  base.id,
+  base_connection.id,
   nodes_ids.node_id
 ) UNION ALL (
-SELECT DISTINCT ON (change.id, nodes_ids.node_id)
-  change.id,
+SELECT DISTINCT ON (changes_connection.id, nodes_ids.node_id)
+  changes_connection.id,
   false AS base,
   nodes_ids.lost_connection,
   nodes_ids.node_id
 FROM
-  change
+  changes_connection
   JOIN nodes_ids USING (node_id)
 ORDER BY
-  change.id,
+  changes_connection.id,
   nodes_ids.node_id
 )
+;
+
+CREATE TEMP VIEW validator_network AS
+SELECT * FROM lost_connection
 ;
