d352a8ff39e3582ef1545b9a4e714e18fcf3f2eb
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2026-03-23 13:44:42 +0100

    Pass srid to validator

--- test/lib/time_machine/validation/validation_test.rb
+++ test/lib/time_machine/validation/validation_test.rb
@@ -104,7 +104,7 @@ class TestValidation < Test::Unit::TestCase
     ).returns(Validation::ValidationResult)
   }
   def validate(validators, prevalidation_clusters)
-    a = Validation.time_machine_validate(nil, validators, prevalidation_clusters).first&.last
+    a = Validation.time_machine_validate(nil, @@srid, validators, prevalidation_clusters).first&.last
     T.must(a&.first&.result)
   end
 
