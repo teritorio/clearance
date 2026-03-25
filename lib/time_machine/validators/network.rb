1819c25ea44a1dec10e28ba73b7aa4f331c7d012
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2026-03-25 22:06:02 +0100

    Lint SQL Validators

--- lib/time_machine/validators/network.rb
+++ lib/time_machine/validators/network.rb
@@ -16,6 +16,31 @@ module Validators
     sig {
       params(
         conn: T.nilable(PG::Connection),
+        before_ids: T::Array[Integer],
+        after_ids: T::Array[Integer],
+      ).returns(T::Array[T::Hash[String, T.untyped]])
+    }
+    def sql(conn, before_ids, after_ids)
+      node_ids = T.let([], T::Array[T::Hash[String, T.untyped]])
+      T.must(conn).transaction { |conn|
+        specific_osm_tags_matches = T.must(@settings.specific_osm_tags_matches)
+        sql_osm_filter_tags = specific_osm_tags_matches.to_sql('postgres', '_', proc { |s| conn.escape_literal(s) })
+        conn.exec(File.new(File.join(File.dirname(__FILE__), 'network.sql')).read
+          .gsub(':osm_filter_tags', sql_osm_filter_tags)
+          .gsub(':base_ways_ids', "ARRAY[#{before_ids.join(',')}]")
+          .gsub(':change_ways_ids', "ARRAY[#{after_ids.join(',')}]"))
+        node_ids = T.cast(conn.exec('SELECT * FROM validator_network').to_a, T::Array[T::Hash[String, T.untyped]])
+        raise 'rollback'
+      }
+    rescue StandardError => e
+      raise unless e.message == 'rollback'
+
+      node_ids
+    end
+
+    sig {
+      params(
+        conn: T.nilable(PG::Connection),
         _proj: Integer,
         prevalidation_clusters: T::Array[[T::Array[Validation::Link], T::Array[Validation::Link]]],
       ).void
@@ -36,21 +61,7 @@ module Validators
       }
 
       # Get node_id that are in change but not in base, and node_id that are in base but not in change
-      node_ids = T.let([], T::Array[T::Hash[String, T.untyped]])
-      begin
-        T.must(conn).transaction { |conn|
-          specific_osm_tags_matches = T.must(@settings.specific_osm_tags_matches)
-          sql_osm_filter_tags = specific_osm_tags_matches.to_sql('postgres', '_', proc { |s| conn.escape_literal(s) })
-          conn.exec(File.new(File.join(File.dirname(__FILE__), 'network.sql')).read
-            .gsub(':osm_filter_tags', sql_osm_filter_tags)
-            .gsub(':base_ways_ids', "ARRAY[#{before_ids.join(',')}]")
-            .gsub(':change_ways_ids', "ARRAY[#{after_ids.join(',')}]"))
-          node_ids = T.cast(conn.exec('SELECT * FROM validator_network').to_a, T::Array[T::Hash[String, T.untyped]])
-          raise 'rollback'
-        }
-      rescue StandardError => e
-        raise unless e.message == 'rollback'
-      end
+      node_ids = sql(conn, before_ids.uniq, after_ids.uniq)
 
       # Flag corresponding way that are disconnected or connected from the neighbor
       prevalidation_clusters.collect{ |_accepted_links, conflations_matches|
