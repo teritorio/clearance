ee28e27a8277600c3bad19f36d6cf09891e8e5b6
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2026-04-22 18:11:35 +0200

    Fix validations_log.changeset_ids to not null

---
+++ db/migrate/20260401000000_validation_changeset_ids_null.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+# typed: false
+
+class ValidationChangesetIdsNull < ActiveRecord::Migration[7.0]
+  def change
+    sql = "SELECT schema_name FROM information_schema.schemata WHERE schema_name NOT IN ('public', 'information_schema') AND schema_name NOT LIKE 'pg_%'"
+    result = ActiveRecord::Base.connection.execute(sql)
+    result.collect{ |row| row['schema_name'] }.each { |schema_name|
+      execute <<~SQL # rubocop:disable Rails/ReversibleMigration
+        SET search_path TO "#{schema_name}", public;
+
+        ALTER TABLE validations_log
+          ALTER COLUMN changeset_ids DROP NOT NULL;
+      SQL
+    }
+  end
+end
