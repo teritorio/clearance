241b01f444cb57772555470554d36a56d487b871
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2026-05-10 14:33:58 +0200

    Compute connex components before clustering

--- lib/time_machine/sql/20_changes_uncibled.sql
+++ lib/time_machine/sql/20_changes_uncibled.sql
@@ -65,6 +65,7 @@ changes_base AS (
         (clip.geom IS NULL OR ST_Intersects(clip.geom, _.geom))
 )
 SELECT
+    row_number() OVER () AS cc_id,
     objtype,
     id,
     base.cibled OR changes.cibled AS cibled,
@@ -88,6 +89,7 @@ END; $$ LANGUAGE plpgsql;
 INSERT INTO changes
 -- changes_with_base
 SELECT
+    coalesce((SELECT max(cc_id) FROM changes), 0) + row_number() OVER () AS cc_id,
     objtype,
     id,
     coalesce(:osm_filter_tags, false) AND
@@ -161,20 +163,6 @@ DO $$ BEGIN
 END; $$ LANGUAGE plpgsql;
 
 
-UPDATE osm_changes
-SET cibled = changes.cibled
-FROM changes
-WHERE
-    osm_changes.objtype = changes.objtype AND
-    osm_changes.id = changes.id AND
-    osm_changes.cibled IS DISTINCT FROM changes.cibled
-;
-
-DO $$ BEGIN
-    RAISE NOTICE '20_changes_uncibled - cibled changes: % / %', (SELECT COUNT(*) FROM changes WHERE cibled), (SELECT COUNT(*) FROM changes);
-END; $$ LANGUAGE plpgsql;
-
-
 CREATE TEMP TABLE osm_changes_members AS
 SELECT
     relations.id AS relation_id,
@@ -199,6 +187,168 @@ DO $$ BEGIN
 END; $$ LANGUAGE plpgsql;
 
 
+-- Propagate cc_id to all connex components by topology
+WITH RECURSIVE a AS (
+    SELECT cc_id, objtype, id FROM changes
+    UNION
+    (
+        WITH
+        -- Recursive term should be referenced only once time
+        b AS (
+            SELECT
+                a.cc_id,
+                changes.objtype,
+                changes.id,
+                changes.nodes,
+                changes.members
+            FROM a
+                JOIN changes USING (objtype, id)
+        ),
+
+        nodes_to_ways AS (
+        SELECT
+            least(min(nodes.cc_id), ways.cc_id) AS cc_id,
+            ways.objtype,
+            ways.id
+        FROM
+            b AS nodes
+            JOIN changes AS ways ON
+                ways.objtype = 'w' AND
+                ways.nodes @> ARRAY[nodes.id]
+        WHERE
+            nodes.objtype = 'n'
+        GROUP BY
+            ways.cc_id,
+            ways.objtype,
+            ways.id
+        ),
+        ways_to_nodes AS (
+        SELECT
+            least(min(ways.cc_id), nodes.cc_id) AS cc_id,
+            nodes.objtype,
+            nodes.id
+        FROM
+            b AS ways
+            JOIN LATERAL unnest(ways.nodes) AS node_id ON true
+            JOIN changes AS nodes ON
+                nodes.objtype = 'n' AND
+                nodes.id = node_id
+        WHERE
+            ways.objtype = 'w'
+        GROUP BY
+            nodes.cc_id,
+            nodes.objtype,
+            nodes.id
+        ),
+
+        relations_to_nodes AS (
+        SELECT
+            least(min(relations.cc_id), nodes.cc_id) AS cc_id,
+            nodes.objtype,
+            nodes.id
+        FROM
+            b AS relations
+            JOIN osm_changes_members AS m ON
+                m.relation_id = relations.id AND
+                m.type = 'n'
+            JOIN changes AS nodes ON
+                nodes.objtype = 'n' AND
+                nodes.id = m.ref
+        WHERE
+            relations.objtype = 'r'
+        GROUP BY
+            nodes.cc_id,
+            nodes.objtype,
+            nodes.id
+        ),
+        relations_to_ways AS (
+        SELECT
+            least(min(relations.cc_id), ways.cc_id) AS cc_id,
+            ways.objtype,
+            ways.id
+        FROM
+            b AS relations
+            JOIN osm_changes_members AS m ON
+                m.relation_id = relations.id AND
+                m.type = 'w'
+            JOIN changes AS ways ON
+                ways.objtype = 'w' AND
+                ways.id = m.ref
+        WHERE
+            relations.objtype = 'r'
+        GROUP BY
+            ways.cc_id,
+            ways.objtype,
+            ways.id
+        ),
+        nodes_or_ways_to_relations AS (
+        SELECT
+            least(min(nodes_or_ways.cc_id), relations.cc_id) AS cc_id,
+            relations.objtype,
+            relations.id
+        FROM
+            b AS nodes_or_ways
+            JOIN osm_changes_members AS m ON
+                m.type IN ('n', 'w') AND
+                m.type = nodes_or_ways.objtype AND
+                m.ref = nodes_or_ways.id
+            JOIN changes AS relations ON
+                relations.objtype = 'r' AND
+                relations.id = m.relation_id
+        WHERE
+            nodes_or_ways.objtype IN ('n', 'w')
+        GROUP BY
+            relations.cc_id,
+            relations.objtype,
+            relations.id
+        )
+        -- TODO relations to relations
+        SELECT * FROM nodes_to_ways
+        UNION ALL
+        SELECT * FROM ways_to_nodes
+        UNION ALL
+        SELECT * FROM relations_to_nodes
+        UNION ALL
+        SELECT * FROM relations_to_ways
+        UNION ALL
+        SELECT * FROM nodes_or_ways_to_relations
+    )
+),
+min_cc_id AS (
+    SELECT objtype, id, min(cc_id) AS cc_id FROM a GROUP BY objtype, id
+)
+UPDATE
+    changes
+SET
+    cc_id = min_cc_id.cc_id
+FROM
+    min_cc_id
+WHERE
+    changes.objtype = min_cc_id.objtype AND
+    changes.id = min_cc_id.id
+;
+
+DROP TABLE osm_changes_members CASCADE;
+
+DO $$ BEGIN
+    RAISE NOTICE '20_changes_uncibled - connex components: %', (SELECT COUNT(DISTINCT cc_id) FROM changes);
+END; $$ LANGUAGE plpgsql;
+
+
+UPDATE osm_changes
+SET cibled = changes.cibled
+FROM changes
+WHERE
+    osm_changes.objtype = changes.objtype AND
+    osm_changes.id = changes.id AND
+    osm_changes.cibled IS DISTINCT FROM changes.cibled
+;
+
+DO $$ BEGIN
+    RAISE NOTICE '20_changes_uncibled - cibled changes: % / %', (SELECT COUNT(*) FROM changes WHERE cibled), (SELECT COUNT(*) FROM changes);
+END; $$ LANGUAGE plpgsql;
+
+
 -- Match grid cells to pairs of integers using Cantor pairing
 DROP FUNCTION IF EXISTS cantor_pairing CASCADE;
 CREATE FUNCTION cantor_pairing(x bigint, y bigint) RETURNS bigint AS $$
@@ -212,175 +362,38 @@ $$ LANGUAGE plpgsql IMMUTABLE;
 
 CREATE TEMP TABLE changes_cluster AS
 WITH
-a AS (SELECT objtype, id, cibled, nodes, members, ST_Transform(ST_MakeValid(geom), :proj) AS geom_proj FROM changes),
-b AS (SELECT objtype, id, cibled, nodes, members, geom_proj, ST_SnapToGrid(ST_PointOnSurface(geom_proj), :distance * 100) AS snap_geom FROM a),
-c AS (SELECT objtype, id, cibled, nodes, members, geom_proj, cantor_pairing(ST_X(snap_geom)::bigint, ST_Y(snap_geom)::bigint) AS snap_grid_id FROM b)
-SELECT
-    objtype, id, cibled, nodes, members,
-    snap_grid_id,
-    ST_ClusterWithinWin(geom_proj, :distance) OVER (PARTITION BY snap_grid_id) AS cluster_id
-FROM c
+a AS (SELECT cc_id, bool_or(cibled) AS cibled, ST_Transform(ST_MakeValid(ST_Collect(geom)), :proj) AS geom_proj FROM changes GROUP BY cc_id),
+b AS (SELECT cc_id, cibled, geom_proj, ST_SnapToGrid(ST_PointOnSurface(geom_proj), :distance * 100) AS snap_geom FROM a),
+c AS (SELECT cc_id, cibled, geom_proj, cantor_pairing(ST_X(snap_geom)::bigint, ST_Y(snap_geom)::bigint) AS snap_grid_id FROM b),
+d AS (SELECT cc_id, cibled, ST_ClusterWithinWin(geom_proj, :distance) OVER (PARTITION BY snap_grid_id) AS cluster_id FROM c),
+e AS (SELECT cc_id, bool_or(cibled) OVER (PARTITION BY cluster_id) AS cibled, cluster_id FROM d)
+SELECT cc_id FROM e WHERE cibled
 ;
-ALTER TABLE changes_cluster ADD PRIMARY KEY (objtype, id);
-CREATE INDEX changes_cluster_idx_nodes ON changes_cluster USING GIN (nodes);
-CREATE INDEX changes_cluster_idx_snap_grid_id_cluster_id ON changes_cluster (snap_grid_id, cluster_id);
-
-DROP TABLE changes;
-
-CREATE TEMP TABLE cibled_changes_cluster AS
-SELECT * FROM changes_cluster WHERE cibled;
-ALTER TABLE cibled_changes_cluster ADD PRIMARY KEY (objtype, id);
+CREATE INDEX changes_cluster_idx_cc_id ON changes_cluster (cc_id);
 
 DO $$ BEGIN
     RAISE NOTICE '20_changes_uncibled - cluster changes: %', (SELECT COUNT(*) FROM changes_cluster);
 END; $$ LANGUAGE plpgsql;
 
 
--- Add transitive changes
-
-INSERT INTO cibled_changes_cluster
-WITH RECURSIVE a AS (
-    SELECT objtype, id FROM cibled_changes_cluster
-    UNION
-    (
-        WITH
-        -- Recursive term should be referenced only once time
-        b AS (
-            SELECT
-                changes_cluster.*
-            FROM a
-                JOIN changes_cluster USING (objtype, id)
-        ),
-
-        by_geom AS (
-        SELECT
-            other.objtype,
-            other.id
-        FROM
-            (SELECT snap_grid_id, cluster_id FROM b GROUP BY snap_grid_id, cluster_id) AS cluster_ids
-            JOIN changes_cluster AS other ON
-                other.snap_grid_id = cluster_ids.snap_grid_id AND
-                other.cluster_id = cluster_ids.cluster_id
-        ),
-
-        nodes_to_ways AS (
-        SELECT
-            ways.objtype,
-            ways.id
-        FROM
-            b AS nodes
-            JOIN changes_cluster AS ways ON
-                ways.objtype = 'w' AND
-                ways.nodes @> ARRAY[nodes.id]
-        WHERE
-            nodes.objtype = 'n'
-        ),
-        ways_to_nodes AS (
-        SELECT
-            nodes.objtype,
-            nodes.id
-        FROM
-            b AS ways
-            JOIN LATERAL unnest(ways.nodes) AS node_id ON true
-            JOIN changes_cluster AS nodes ON
-                nodes.objtype = 'n' AND
-                nodes.id = node_id
-        WHERE
-            ways.objtype = 'w'
-        ),
-
-        relations_to_nodes AS (
-        SELECT
-            nodes.objtype,
-            nodes.id
-        FROM
-            b AS relations
-            JOIN osm_changes_members AS m ON
-                m.relation_id = relations.id AND
-                m.type = 'n'
-            JOIN changes_cluster AS nodes ON
-                nodes.objtype = 'n' AND
-                nodes.id = m.ref
-        WHERE
-            relations.objtype = 'r'
-        ),
-        relations_to_ways AS (
-        SELECT
-            ways.objtype,
-            ways.id
-        FROM
-            b AS relations
-            JOIN osm_changes_members AS m ON
-                m.relation_id = relations.id AND
-                m.type = 'w'
-            JOIN changes_cluster AS ways ON
-                ways.objtype = 'w' AND
-                ways.id = m.ref
-        WHERE
-            relations.objtype = 'r'
-        ),
-        nodes_or_ways_to_relations AS (
-        SELECT
-            relations.objtype,
-            relations.id
-        FROM
-            b AS nodes_or_ways
-            JOIN osm_changes_members AS m ON
-                m.type IN ('n', 'w') AND
-                m.type = nodes_or_ways.objtype AND
-                m.ref = nodes_or_ways.id
-            JOIN changes_cluster AS relations ON
-                relations.objtype = 'r' AND
-                relations.id = m.relation_id
-        WHERE
-            nodes_or_ways.objtype IN ('n', 'w')
-        )
-        SELECT * FROM by_geom
-        UNION ALL
-        SELECT * FROM nodes_to_ways
-        UNION ALL
-        SELECT * FROM ways_to_nodes
-        UNION ALL
-        SELECT * FROM relations_to_nodes
-        UNION ALL
-        SELECT * FROM relations_to_ways
-        UNION ALL
-        SELECT * FROM nodes_or_ways_to_relations
-    )
-)
-SELECT
-    changes_cluster.*
-FROM
-    a
-    JOIN changes_cluster USING (objtype, id)
-ON CONFLICT DO NOTHING
-;
-
-DROP TABLE osm_changes_members CASCADE;
-DROP TABLE changes_cluster CASCADE;
-
-DO $$ BEGIN
-    RAISE NOTICE '20_changes_uncibled - cibled_changes & transitive: %', (SELECT COUNT(*) FROM cibled_changes_cluster);
-END; $$ LANGUAGE plpgsql;
-
 -- Select only changes not related to objects and area of interest, and not transitively related to them
 DROP TABLE IF EXISTS changes_update;
 CREATE TEMP TABLE changes_update AS
-SELECT DISTINCT ON (osm_changes.objtype, osm_changes.id)
+SELECT
     osm_changes.*
 FROM
     osm_changes
-    LEFT JOIN cibled_changes_cluster AS cibled ON
-        cibled.objtype = osm_changes.objtype AND
-        cibled.id = osm_changes.id
+    JOIN changes ON
+        changes.objtype = osm_changes.objtype AND
+        changes.id = osm_changes.id
+    LEFT JOIN changes_cluster ON
+        changes_cluster.cc_id = changes.cc_id
 WHERE
-    cibled.objtype IS NULL
-ORDER BY
-    osm_changes.objtype,
-    osm_changes.id
+    changes_cluster.cc_id IS NULL
 ;
 
-DROP TABLE cibled_changes_cluster CASCADE;
+DROP TABLE changes;
+DROP TABLE changes_cluster CASCADE;
 
 DO $$ BEGIN
     RAISE NOTICE '20_changes_uncibled - changes_update: %', (SELECT COUNT(*) FROM changes_update);
