461d94897d040e89beabb72cb8b7d860d6ae2ce6
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2026-07-03 18:34:55 +0200

    Update changeset on a log bias

--- lib/time_machine/db/changeset.rb
+++ lib/time_machine/db/changeset.rb
@@ -17,7 +17,10 @@ module Db
       INSERT INTO
         osm_changesets
       VALUES
-        ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13)
+        ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, now())
+      ON CONFLICT (id)
+      DO UPDATE SET
+        (id, created_at, closed_at, open, \"user\", uid, min_lat, min_lon, max_lat, max_lon, comments_count, changes_count, tags, updated_at) = (EXCLUDED.id, EXCLUDED.created_at, EXCLUDED.closed_at, EXCLUDED.open, EXCLUDED.\"user\", EXCLUDED.uid, EXCLUDED.min_lat, EXCLUDED.min_lon, EXCLUDED.max_lat, EXCLUDED.max_lon, EXCLUDED.comments_count, EXCLUDED.changes_count, EXCLUDED.tags, EXCLUDED.updated_at)
     ")
 
     sql = "
@@ -28,10 +31,11 @@ module Db
       JOIN osm_changes ON
         osm_changes.objtype = osm_base.objtype AND
         osm_changes.id = osm_base.id
-        LEFT JOIN osm_changesets ON
-          osm_changesets.id = osm_base.changeset_id
+      LEFT JOIN osm_changesets ON
+        osm_changesets.id = osm_base.changeset_id
     WHERE
-      osm_changesets.id IS NULL
+      osm_changesets.id IS NULL OR
+      osm_changesets.open
 
     UNION
 
@@ -42,7 +46,11 @@ module Db
       LEFT JOIN osm_changesets ON
         osm_changesets.id = osm_changes.changeset_id
     WHERE
-      osm_changesets.id IS NULL
+      osm_changesets.id IS NULL OR
+      osm_changesets.open OR
+      (extract(EPOCH FROM (now() - updated_at))) >=
+        -- 6h, 12h, 1d, 2d, 4d...
+        6 * power(2, floor(log(2, greatest(0, (extract(EPOCH FROM (updated_at - created_at)) / 3600.0 / 6)) + 1))) * 3600
     "
 
     changeset_ids = conn.exec(sql).pluck('id').compact
