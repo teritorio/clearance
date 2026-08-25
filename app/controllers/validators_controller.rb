d15f307dc914b98cd0c95ed22b02517fa1739ba9
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2026-08-25 15:33:19 +0200

    Fix validation API

--- app/controllers/validators_controller.rb
+++ app/controllers/validators_controller.rb
@@ -9,10 +9,7 @@ class ValidatorsController < ApplicationController
   def index
     project = params['project'].to_s
 
-    json = ::Configuration.load("/#{Project.projects_config_path}/#{project}/config.yaml").validators.to_h{ |validator|
-      h = validator.to_h
-      [h['id'], h.except('id')]
-    }
+    json = ::Configuration.load("/#{Project.projects_config_path}/#{project}/config.yaml").validators
     render json: json
   end
 end
